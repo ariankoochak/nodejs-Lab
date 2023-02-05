a401352eb783c62aa874dcf5bbc2ba70f158da31
Arian <arian.koochak@protonmail.com>
2023-02-05 08:08:32 +0330

    season 1-bubble sort in node

--- Season 1/Bubble Sort/index.js
+++ Season 1/Bubble Sort/index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const myArr = [1,6,3,2,5,7,4,9];
+
+for(let i = 0;i < myArr.length;i++){
+    for(let j = 0;j < myArr.length;j++){
+        if(myArr[i] < myArr[j]){
+            let tmp = myArr[i];
+            myArr[i] = myArr[j];
+            myArr[j] = tmp;
+        }
+    }
+}
+
+console.log(myArr);
