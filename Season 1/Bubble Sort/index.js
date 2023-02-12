c8db053c63d9417ec191364b268da7b645da79fb
Arian <arian.koochak@protonmail.com>
2023-02-12 16:29:33 +0330

    comment-not complete

--- Season 1/Bubble Sort/index.js
+++ Season 1/Bubble Sort/index.js
@@ -11,3 +11,12 @@ for(let i = 0;i < myArr.length;i++){
 }
 
 console.log(myArr);
+
+
+// for run with Node =>                        node index.js
+// for run with nodemon =>                     nodemon index.js 
+// for run with nodemon in VScode terminal =>  npx nodemon index.js
+// for install nodemon Global=>                npm install -g nodemon
+// for install nodemon just in project =>      npm install --save-dev nodemon
+
+// for more details for npm & npx : https://roocket.ir/articles/npm-vs-npx-whats-the-difference
--- Season 2/Sample Server/server.js
+++ Season 2/Sample Server/server.js
@@ -1,13 +1,15 @@
-const http = require('http');
+const http = require('http'); //this module installed with node & use for make server(Line 7)
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1'; 
+const port = 3000;  
+//We want our server to run when sending requests to this IP(hostname) and port(port)
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{ //Whenever a request is sent to this server, this function is executed
     res.setHeader('content-type','text/plain');
     res.end('hello from text content');
 })
 
 server.listen(port,hostname,(req,res)=>{
-    console.log("hello");
+    console.log("hello");  
 })
+
