cc92ecdc56cdfb4dc5750e26b9ca1f987d66e007
Arian <45326611+ariankoochak@users.noreply.github.com>
2023-02-25 10:43:30 +0330

    fix personal website

--- Season 4/Express-js-personal-website/index.js
+++ Season 4/Express-js-personal-website/index.js
@@ -5,10 +5,6 @@ const port = 3000
 
 app.use('/', router)
 
-app.use((req, res) => {
-  res.status(404).send('mimiri mesl adam request bezani??')
-})
-
 app.use((err, req, res, next) => { // error handling
   console.error(err.stack)
   res.status(500).send('500 yani ridam vali nemidoonam koja!')
