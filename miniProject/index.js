4a841d9adc8f5fe546d9cdfdabd3708b81b1bf28
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-07 14:14:25 +0330

    add first routing

--- miniProject/index.js
+++ miniProject/index.js
@@ -1,8 +1,18 @@
 const http = require('http')
+const products = require('./data/products.json')
 const port = 3000
 
 const server = http.createServer((req,res)=>{
-    console.log("request received!");
+    if (req.url === "/api/products") {
+        res.writeHead(200,{'Content-Type' : 'application/json'})
+        res.write(JSON.stringify(products))
+        res.end()
+    }
+    else{
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({message : 'api not found!'}));
+        res.end()
+    }
 })
 
 server.listen(port,()=>{
